b971e59fd0e3ec4fc57427e0cc721f3182a54232
Al <al1102@yandex.ru>
2018-06-14 19:57:53 +0500

    up

--- app/__init__.py
+++ app/__init__.py
@@ -1,22 +1,7 @@
-'''
-UPD: не понадобилось
-__init__.py is imported using a directory. if you want to import it as app you
-should put __init__.py file in directory named app
-a better option is just to rename __init__.py to app.py
-'''
-
-
 from flask import Flask
+from config import Config
 
 app = Flask(__name__)
-app.config.from_object('config')
+app.config.from_object(Config)
 
-from app import views
-'''
-UPD: не понадобилось
-There is no need to import the views in your project-level file. You are not
-using them there, so no reason to import them.
-If you did need to, you would just to from blog import views, because the views
-are in the blog directory and manage.py puts the top-level directory into the
-Python path.
-'''
+from app import routes
